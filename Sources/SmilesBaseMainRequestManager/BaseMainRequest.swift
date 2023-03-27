9f1b329c811c949768408773913962fc5edf28f6
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-03-27 12:25:40 +0500

    - Initial setup

---
+++ Sources/SmilesBaseMainRequestManager/BaseMainRequest.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 21/02/2023.
+//
+
+import Foundation
+
+//open class SmilesBaseMainRequest: Codable {
+//
+//    public init() {}
+//
+//    open func encode(to encoder: Encoder) throws {
+//        guard let configs = SmilesBaseMainRequestManager.shared.baseMainRequestConfigs else { return }
+//        try configs.encode(to: encoder)
+//    }
+//
+//}
