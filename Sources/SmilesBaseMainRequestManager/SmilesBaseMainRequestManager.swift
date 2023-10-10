40ef84d1b1c2d887ce3f6c17dec5c37977c20a9f
Naguib <ahmedmaelesawy@gmail.com>
2023-10-10 13:21:50 +0300

    Solve

--- Sources/SmilesBaseMainRequestManager/SmilesBaseMainRequestManager.swift
+++ Sources/SmilesBaseMainRequestManager/SmilesBaseMainRequestManager.swift
@@ -11,7 +11,21 @@ import SmilesUtilities
 @objc public class SmilesBaseMainRequestManager: NSObject {
     
     public static let shared = SmilesBaseMainRequestManager()
-    public var baseMainRequestConfigs: SmilesBaseMainRequest?
+    private let serialQueue = DispatchQueue(label: "com.example.smilesSerialQueue", attributes: .concurrent)
+    private var _baseMainRequestConfigs: SmilesBaseMainRequest?
+    
+    public var baseMainRequestConfigs: SmilesBaseMainRequest? {
+       get {
+           return serialQueue.sync {
+                _baseMainRequestConfigs
+           }
+       }
+       set {
+           serialQueue.async {
+               self._baseMainRequestConfigs = newValue
+           }
+       }
+   }
     
     @objc public func setupBaseRequestConfigs(additionalInfo: [BaseMainResponseAdditionalInfo], appVersion: String?, authToken: String?, channel: String?, deviceId: String?, handsetModel: String?, imsi: String?, isGpsEnabled: Bool = false, isNotificationEnabled: Bool = false, langauge: String?, msisdn: String?, osVersion: String?, token: String?, hashId: String?, deviceHashId: String?, userInfo: AppUserInfo?, deviceHashIdV2: String?) {
         
